d99f75a687147a07e52dd624bf64fd2c14767cd9
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-15 16:13:16 -0300

    create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <div className="App">
-      <h1>I'm God in action!</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      
+      <div className="App">
+        <h1>I'm God in action!</h1>
+      </div>
+    </>
   )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
+    --red: #E52E4D;
+    --blue: #5429CC;
+    --blue-light: #6933FF;
+    --green: #33CC95;
+    --text-title: #363F5F;
+    --text-body: #969CB2;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disable] {
+    opacity: 0.6;
+    
+    cursor: not-allowed;
+  }
+`
