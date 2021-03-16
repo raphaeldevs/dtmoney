1f0e40d6605c1c1ef9be8f4754afb9e1b906b1f3
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-16 13:42:52 -0300

    add seeds for transactions in miragejs db

--- src/index.tsx
+++ src/index.tsx
@@ -10,11 +10,42 @@ createServer({
     transaction: Model
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Resgate CDI',
+          amount: 12000,
+          type: 'deposit',
+          category: 'Investimentos',
+          createdAt: new Date('2021-03-16 08:01:49')
+        },
+        {
+          id: 2,
+          title: 'Resgate CDI',
+          amount: 5000,
+          type: 'deposit',
+          category: 'Investimentos',
+          createdAt: new Date('2021-03-16 08:03:14')
+        },
+        {
+          id: 3,
+          title: 'Pagamento Diego',
+          amount: 15000,
+          type: 'withdraw',
+          category: 'Transferência',
+          createdAt: new Date('2021-03-16 10:12:43')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api'
 
     this.get('/transactions', () => {
-      return this.schema.all('transactions')
+      return this.schema.all('transaction')
     })
 
     this.post('/transactions', (schema, request) => {
