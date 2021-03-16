35ca0421756c5cebd15f666e2abb03ba0b92a321
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-16 01:27:59 -0300

    create global styles for modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -52,4 +52,36 @@ export const GlobalStyle = createGlobalStyle`
     
     cursor: not-allowed;
   }
+
+  .react-modal-overlay {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: rgba(0, 0, 0, 0.5)
+  }
+
+  .react-modal-content {
+    max-width: 576px;
+    width: 100%;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    padding: 3rem;
+
+    background: var(--background);
+
+    border-radius: 0.25rem;
+
+    position: relative;
+  }
 `
