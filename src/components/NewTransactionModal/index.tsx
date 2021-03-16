f3331d03108ed857f575603c441c696be84d7b24
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-16 01:28:56 -0300

    add body markup to new transation modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -14,11 +14,23 @@ export function NewTransactionModal({
   onRequestClose
 }: NewTransactionModalProps) {
   return (
-    <Container>
-      <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}
-      />
-    </Container>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input placeholder="Título" />
+
+        <input type="number" placeholder="Valor" />
+
+        <input placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
   )
 }
