f0976fb3ee44a186e9de1009e3b25e9b747f134e
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-21 20:08:53 -0300

    feat: edit modal

---
+++ src/components/EditTransactionModal/index.tsx
@@ -0,0 +1,116 @@
+import { FormEvent, useEffect, useState } from 'react'
+
+import Modal from 'react-modal'
+
+import { useTransactions } from '../../hooks/useTransactions'
+
+import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+
+import { Container, TransactionTypeContainer, Button } from './styles'
+import { useTransactionModal } from '../../hooks/useTransactionModal'
+
+Modal.setAppElement('#root')
+
+export function EditTransactionModal() {
+  const { editTransaction, editingTransaction } = useTransactions()
+  const { isEditTransationModalOpen, handleCloseModal } = useTransactionModal()
+
+  const [title, setTitle] = useState('')
+  const [amount, setAmount] = useState(0)
+  const [category, setCategory] = useState('')
+  const [type, setType] = useState('')
+
+  useEffect(() => {
+    setAmount(editingTransaction.amount)
+    setTitle(editingTransaction.title)
+    setType(editingTransaction.type)
+    setCategory(editingTransaction.category)
+  }, [editTransaction])
+
+  function onRequestClose() {
+    handleCloseModal('editTransaction')
+  }
+
+  async function handleEditTransaction(event: FormEvent) {
+    event.preventDefault()
+
+    await editTransaction(editingTransaction.id, {
+      title,
+      amount,
+      category,
+      type
+    })
+
+    setTitle('')
+    setAmount(0)
+    setCategory('')
+    setType('deposit')
+    onRequestClose()
+  }
+
+  return (
+    <Modal
+      isOpen={isEditTransationModalOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        className="react-modal-close"
+        onClick={onRequestClose}
+      >
+        <img src={closeImg} alt="Fechar modal" title="Fechar modal" />
+      </button>
+
+      <Container onSubmit={handleEditTransaction}>
+        <h2>Editar transação</h2>
+
+        <input
+          placeholder="Título"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
+
+        <input
+          type="number"
+          placeholder="Valor"
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
+        />
+
+        <TransactionTypeContainer>
+          <Button
+            type="button"
+            onClick={() => setType('deposit')}
+            isActive={type === 'deposit'}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </Button>
+
+          <Button
+            type="button"
+            onClick={() => setType('withdraw')}
+            isActive={type === 'withdraw'}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saida" />
+            <span>Saída</span>
+          </Button>
+        </TransactionTypeContainer>
+
+        <input
+          placeholder="Categoria"
+          value={category}
+          onChange={event => setCategory(event.target.value)}
+        />
+
+        <button type="submit">Salvar alterações</button>
+      </Container>
+    </Modal>
+  )
+}
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,4 +1,5 @@
-import { useTransactions } from '../../hooks/useTransactions'
+import { Transaction, useTransactions } from '../../hooks/useTransactions'
+import { useTransactionModal } from '../../hooks/useTransactionModal'
 
 import { formatMoney, formatDate } from '../../utils'
 
@@ -8,8 +9,22 @@ import deleteImg from '../../assets/delete.svg'
 import { Container } from './styles'
 
 export function TransactionsTable() {
-  const { transactions } = useTransactions()
-  
+  const {
+    transactions,
+    deleteTransaction,
+    setCurrentEditingTransaction
+  } = useTransactions()
+  const { handleOpenModal } = useTransactionModal()
+
+  function handleEditModal(transaction: Transaction) {
+    handleOpenModal('editTransaction')
+    setCurrentEditingTransaction(transaction)
+  }
+
+  function handleDeleteTransaction(id: number) {
+    deleteTransaction(id)
+  }
+
   return (
     <Container>
       <table>
@@ -28,19 +43,27 @@ export function TransactionsTable() {
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
               <td className={transaction.type}>
-                {formatMoney(transaction.amount)}
+                {transaction.type === 'withdraw'
+                  ? formatMoney(transaction.amount * -1)
+                  : formatMoney(transaction.amount)}
               </td>
               <td>{transaction.category}</td>
+              <td>{formatDate(transaction.createdAt)}</td>
               <td>
-                {formatDate(new Date(transaction.createdAt))}
-              </td>
-              <td>
-                <button type="button" title="Editar transação">
-                  <img src={editImg} alt="caneta"/>
+                <button
+                  type="button"
+                  title="Editar transação"
+                  onClick={() => handleEditModal(transaction)}
+                >
+                  <img src={editImg} alt="caneta" />
                 </button>
 
-                <button type="button" title="Excluir transação">
-                  <img src={deleteImg} alt="lixeira"/>
+                <button
+                  type="button"
+                  title="Excluir transação"
+                  onClick={() => handleDeleteTransaction(transaction.id)}
+                >
+                  <img src={deleteImg} alt="lixeira" />
                 </button>
               </td>
             </tr>
