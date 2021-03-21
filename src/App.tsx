7c5574bb33a861fc434c6db1a0ea5c6d3f75b69e
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-21 20:10:32 -0300

    add TransactionsModalProvider in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
-import { useState } from 'react'
-
 import { GlobalStyle } from './styles/global'
 
 import { TransactionsProvider } from './hooks/useTransactions'
 
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
-import { NewTransactionModal } from './components/NewTransactionModal'
+import { TransactionsModalProvider } from './hooks/useTransactionModal'
 
 export function App() {
-  const [isNewTransationModalOpen, setIsNewTransationModalOpen] = useState(false)
-
-  function handleOpenNewTransactionModal() {
-    setIsNewTransationModalOpen(true)
-  }
-
-  function handleCloseNewTransactionModal() {
-    setIsNewTransationModalOpen(false)
-  }
-
   return (
     <TransactionsProvider>
-      <GlobalStyle />
-
-      <NewTransactionModal
-        isOpen={isNewTransationModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-      />
-
-      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
-
-      <Dashboard />
+      <TransactionsModalProvider>
+      
+        <GlobalStyle />
+        <Header />
+        
+        <Dashboard />
+      </TransactionsModalProvider>
     </TransactionsProvider>
   )
 }
